e63ab7325116dd71db6f5032e548738669b4d173
= <=>
2016-03-27 12:20:05 -0400

    Add a relationship to the database

---
+++ doremi/doremi.sql
@@ -0,0 +1,40 @@
+DROP DATABASE IF EXISTS `DoReMi`;
+CREATE DATABASE `DoReMi`
+	DEFAULT CHARACTER SET utf8
+	DEFAULT COLLATE utf8_general_ci;
+USE `DoReMi`;
+
+CREATE TABLE `players`(
+	`player_id`	varchar(5) NOT NULL,
+	`player_key` varchar(128) NOT NULL,
+	`created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`player_id`)
+);
+
+CREATE TABLE `difficulties`(
+	`difficulty` varchar(6) NOT NULL,
+	PRIMARY KEY (`difficulty`)
+);
+
+CREATE TABLE `times`(
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`player_id` varchar(5) NOT NULL,
+	`difficulty` varchar(6) NOT NULL,
+	`start_time` datetime NOT NULL,
+	`end_time` datetime NOT NULL,
+	`created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`),
+	KEY `player_id` (`player_id`),
+	CONSTRAINT `player_id_ibfk_1`  
+		FOREIGN KEY (`player_id`) 
+		REFERENCES `players` (`player_id`)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+	KEY `difficulty` (`difficulty`),
+	CONSTRAINT `difficulty_ibfk_1` 
+		FOREIGN KEY (`difficulty`) 
+		REFERENCES `difficulties` (`difficulty`)
+);
+
+INSERT INTO `difficulties` VALUES ('easy');
+INSERT INTO `difficulties` VALUES ('medium');
+INSERT INTO `difficulties` VALUES ('hard');
